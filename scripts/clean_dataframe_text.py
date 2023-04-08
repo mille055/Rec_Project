616bf8a27d20bdb63fb4b479f05f73c708a3a87f
mille055 <46400739+mille055@users.noreply.github.com>
2023-04-08 10:53:52 -0400

    updated dataframes and docs in clean_dataframe

--- scripts/clean_dataframe_text.py
+++ scripts/clean_dataframe_text.py
@@ -12,6 +12,13 @@ from tqdm import tqdm
 
 
 def convert_si_to_number(x):
+    '''
+    Converts a number of star ratings to integer (e.g., 10M = 10,000)
+    Input: 
+        x(str): number with M,K etc
+    Ouput:
+        int(total_stars) (int): corresponding integer
+    '''
     total_stars = 0
     if 'K' in x:
         if len(x) > 1:
@@ -27,6 +34,14 @@ def convert_si_to_number(x):
 
 
 def clean_title(t):
+    '''
+    Cleans the text of the title column
+    Input: 
+        t(str): title that may have special characters, etc
+    Ouput:
+        t(str): cleaned title
+    '''
+
     t = unidecode.unidecode(t)
     t = t.replace('\n', ' ')
     t = re.sub(r'[^\w\s]', '', t)
@@ -35,40 +50,45 @@ def clean_title(t):
     t = t.strip()
     return t
 
-def clean_description(d):
-    d = unidecode.unidecode(d)
-    d = d.replace('\n', ' ')
-    d = re.sub(r'[^\w\s]', '', d)
-    d = re.sub(r'\d+', '', d)
-    if re.findall(r'(.*) brought to you by.*', d):
-      d = re.sub(r'brought to you by.*', '', d)
-    if re.search(r'(.*) sponsored by.*', d):
-      d = re.sub(r'sponsored by.*', '', d)
-    d = d.lower()
-    d = d.strip()
+# def clean_description(d):
+#     d = unidecode.unidecode(d)
+#     d = d.replace('\n', ' ')
+#     d = re.sub(r'[^\w\s]', '', d)
+#     d = re.sub(r'\d+', '', d)
+#     if re.findall(r'(.*) brought to you by.*', d):
+#       d = re.sub(r'brought to you by.*', '', d)
+#     if re.search(r'(.*) sponsored by.*', d):
+#       d = re.sub(r'sponsored by.*', '', d)
+#     d = d.lower()
+#     d = d.strip()
     
-    return d
+#     return d
 
 def clean_description_list(dlist):
-    
+     '''
+    Cleans the text of the episode_descriptions column which is a list of strings
+    Input: 
+        dlist(list of str): text that may have special characters, etc
+    Ouput:
+        new_string(str): concatenates cleaned text into a single string
+    '''
     new_string = ""
     for d in dlist:
-      d = unidecode.unidecode(d)
-      d = d.replace('\n', ' ')
-      d = re.sub(r'[^\w\s]', '', d)
-      d = re.sub(r'\d+', '', d)
-      if re.findall(r'(.*) brought to you by.*', d):
-        d = re.sub(r'brought to you by.*', '', d)
-      if re.search(r'(.*) sponsored by.*', d):
-        d = re.sub(r'sponsored by.*', '', d)
-      d = d.lower()
-      d = d.strip()
+      d = clean_text(d)
       new_string = new_string + " " + d
     return new_string
 
 
 # Preferred method for cleaning the text currently
 def clean_text(text):
+    '''
+    Cleans a single string block of text, removes url's
+    Input: 
+        text(str): text that may have special characters, etc
+    Ouput:
+        text(str): cleans text of special characters
+    '''
+
     text = unidecode.unidecode(text)
     text = text.replace('\n', ' ')
     # Remove URLs
@@ -90,6 +110,14 @@ def join_strings(string_list, separator=" "):
 
 # Combine the join and clean functions to apply to a column of a dataframe
 def join_and_clean_text(string_list, separator=" "):
+    '''
+    Joins a list of strings into a single string and cleans text
+    Input: 
+        string_list(list of str): text that may have special characters, etc
+        separator(str): what to place between the joined strings
+    Ouput:
+        cleaned_text(str): concatenates cleaned text into a single string
+    '''
     # Join the list of strings
     single_text = join_strings(string_list, separator)
 
